4e16b382b8c4068cdf1aeb4ee93332f81b5b8427
Tobias Felix Egger <egtobi@gmail.com>
2020-01-20 18:34:23 +0100

    Fixed Makefile

--- script.bat
+++ script.bat
@@ -1,4 +1,5 @@
 scp %1.c user@193.170.192.225:/home/user/Treiber
+scp Makefile user@193.170.192.225:/home/user/Treiber
 ssh user@193.170.192.225
 scp user@193.170.192.225:/home/user/Treiber/%1.ko %1.ko
 scp %1.ko root@%2:/home/root/Treiber/
